14a7c61a23799b4e823dd03d15f201178a7403b4
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2024-01-04 14:38:26 +0300

    feat: module6 task1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,22 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberofPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberofPaws, boolean hasFur) {
+        this.color = color;
+        this.numberofPaws = numberofPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String far = this.hasFur ? "a" : "no";
+
+        return "This animal is mostly " + this.color +
+            ". It has " + this.numberofPaws +
+            " paw" + (numberofPaws > 1 ? "s" : "") +
+            " and " + far + " fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,11 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+    public Dog() {
+        super("brown",4, true);
+    }
 
+    public static void main(String[] args) {
+        System.out.println(new Dog().getDescription());
+    }
 }
