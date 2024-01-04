76f755f5cf69bfdcbf3f1d964a58f78dc92ad194
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2024-01-04 14:44:28 +0300

    feat: correct animal property name

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,12 +2,12 @@ package com.epam.mjc.stage0;
 
 public class Animal {
     private String color;
-    private int numberofPaws;
+    private int numberOfPaws;
     private boolean hasFur;
 
     public Animal(String color, int numberofPaws, boolean hasFur) {
         this.color = color;
-        this.numberofPaws = numberofPaws;
+        this.numberOfPaws = numberofPaws;
         this.hasFur = hasFur;
     }
 
@@ -15,8 +15,8 @@ public class Animal {
         String far = this.hasFur ? "a" : "no";
 
         return "This animal is mostly " + this.color +
-            ". It has " + this.numberofPaws +
-            " paw" + (numberofPaws > 1 ? "s" : "") +
+            ". It has " + this.numberOfPaws +
+            " paw" + (numberOfPaws > 1 ? "s" : "") +
             " and " + far + " fur.";
     }
 }
